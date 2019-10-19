a8bb447068667dedb77fd8fc0664f2ca6541a2aa
Natasha <nathy-ac@htomail.com>
2019-10-18 23:50:51 -0300

    Inicio do front-end

---
+++ projBlog/src/app/app-routing.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+import { HomeComponent } from './home/home.component';
+import { ContatoComponent } from './contato/contato.component';
+import { SobreComponent } from './sobre/sobre.component';
+
+
+const routes: Routes = [
+  { path: '', component: HomeComponent},
+  { path: 'contato', component: ContatoComponent },
+  { path: 'sobre', component: SobreComponent }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ projBlog/src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { TopoComponent } from './topo/topo.component';
+import { RodapeComponent } from './rodape/rodape.component';
+import { HomeComponent } from './home/home.component';
+import { SobreComponent } from './sobre/sobre.component';
+import { ContatoComponent } from './contato/contato.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    TopoComponent,
+    RodapeComponent,
+    HomeComponent,
+    SobreComponent,
+    ContatoComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
